4b5cffb47668c91efa4ace7b7701a8a91280b3a9
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2022-01-18 14:10:25 -0500

    current and future weather complete...need to add more functionality

--- src/app/future/future.component.ts
+++ src/app/future/future.component.ts
@@ -10,6 +10,14 @@ import { pluck } from 'rxjs/operators';
 })
 export class FutureComponent implements OnInit {
 
+  weatherData:any = [];
+  forecastDetails:any;
+
+  primaryDisplay = true;
+  secondaryDisplay = false;
+
+  selectedIndex:number;
+
   constructor(private forecastService: ForecastService) { }
 
   ngOnInit(): void {
@@ -23,7 +31,21 @@ export class FutureComponent implements OnInit {
 
   futureForecast(data:any){
     for(let i = 0; i< data.length; i = i + 8 ){
+      this.weatherData.push(data[i]);
     }
+    console.log(this.weatherData);
   }
 
+  toggle(data:any, index:number){
+    this.primaryDisplay = !this.primaryDisplay;
+    this.secondaryDisplay = !this.secondaryDisplay;
+
+    this.forecastDetails = data;
+    this.selectedIndex = index;
+  }
+
+  showDetails(data:any, i:number){
+    this.forecastDetails = data;
+    this.selectedIndex = i;
+}
 }
