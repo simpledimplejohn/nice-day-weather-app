84bbe995e6a68e9939376062c0e14d8c3e7283d9
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2022-01-18 20:27:27 -0500

    removed search functionlity

--- src/app/findweather/findweather.component.spec.ts
+++ src/app/findweather/findweather.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FindweatherComponent } from './findweather.component';
+// import { FindweatherComponent } from './findweather.component';
 
-describe('FindweatherComponent', () => {
-  let component: FindweatherComponent;
-  let fixture: ComponentFixture<FindweatherComponent>;
+// describe('FindweatherComponent', () => {
+//   let component: FindweatherComponent;
+//   let fixture: ComponentFixture<FindweatherComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ FindweatherComponent ]
-    })
-    .compileComponents();
-  });
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ FindweatherComponent ]
+//     })
+//     .compileComponents();
+//   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FindweatherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(FindweatherComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
